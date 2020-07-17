c368591fe919da94703a5daa4ca6c26f9b4740af
sanraith <zsjaks@gmail.com>
2020-07-17 12:50:41 +0200

    Simplified config loading.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 
 namespace keep_headset_awake
 {
@@ -8,28 +10,36 @@ namespace keep_headset_awake
     {
         async static Task Main(string[] args)
         {
+            var configFilePath = Path.Join(Directory.GetParent(AppContext.BaseDirectory).FullName, "appsettings.json");
             var config = new BeepConfig();
-            new ConfigurationBuilder()
-                .SetBasePath(System.IO.Directory.GetParent(AppContext.BaseDirectory).FullName)
-                .AddJsonFile("appsettings.json", false)
-                .Build()
-                .Bind(config);
-
+            try
+            {
+                var configContent = File.ReadAllText(configFilePath, Encoding.UTF8);
+                config = JsonSerializer.Deserialize<BeepConfig>(configContent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Could not load appsettings.json!");
+            }
             await new Program { myConfig = config }.RunForever();
         }
 
         private async Task RunForever()
         {
-            var beepFrequency = myConfig.BeepFrequency;
+            var beepFrequency = myConfig.BeepFrequencyHz;
             var beepLength = TimeSpan.FromSeconds(myConfig.BeepDurationSeconds);
             var beepInterval = TimeSpan.FromSeconds(myConfig.BeepIntervalSeconds);
 
+            Console.WriteLine($"Started: {DateTime.Now}");
+            Console.WriteLine($"Frequency: {beepFrequency} Hz, Duration: {beepLength}, Interval: {beepInterval}");
             Console.WriteLine();
+
             while (true)
             {
-                WriteStatus($"Beeping     until {DateTime.Now + beepLength:HH:mm:ss} at {beepFrequency} Hz.");
+                WriteStatus($"Currently beeping until {DateTime.Now + beepLength:HH:mm:ss}.");
                 Console.Beep(beepFrequency, (int)beepLength.TotalMilliseconds);
-                WriteStatus($"Not beeping until {DateTime.Now + beepInterval:HH:mm:ss} at {beepFrequency} Hz.");
+                WriteStatus($"Currently not beeping until {DateTime.Now + beepInterval:HH:mm:ss}.");
                 await Task.Delay(beepInterval);
             }
         }
@@ -47,8 +57,8 @@ namespace keep_headset_awake
 
     public sealed class BeepConfig
     {
-        public int BeepFrequency { get; set; }
-        public int BeepDurationSeconds { get; set; }
-        public int BeepIntervalSeconds { get; set; }
+        public int BeepFrequencyHz { get; set; } = 18000;
+        public int BeepDurationSeconds { get; set; } = 60;
+        public int BeepIntervalSeconds { get; set; } = 240;
     }
 }
